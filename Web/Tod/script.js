7045706543a060e7d91990c4665d36c879c891c9
Kanav Mahajan <kanavmahj@gmail.com>
2024-05-18 20:36:31 +0530

    Updated adding and editing functionality

--- Web/Tod/script.js
+++ Web/Tod/script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask() {
   const task = inputBox.value.trim();
   if (!task) {
@@ -19,6 +20,7 @@ function addTask() {
   listContainer.appendChild(li);
   inputBox.value = "";
 }
+
 const checkbox = li.querySelector("input");
 const editBtn = li.querySelector(".edit-btn");
 const taskSpan = li.querySelector("span");
@@ -26,23 +28,19 @@ const deleteBtn = li.querySelector(".delete-btn");
 
 checkbox.addEventListener("click", function () {
   li.classList.toggle("completed", checkbox.checked);
-
-  updateCounters();
 });
+
 editBtn.addEventListener("click", function () {
   const update = prompt("Edit task:", taskSpan.textContent);
   if (update !== null) {
     taskSpan.textContent = update;
     li.classList.remove("completed");
-
-    checkbox.checked=false;
-    updateCounters();
   }
 });
 
-
 const completedCounter = document.getElementById("completed-counter");
 const uncompletedCounter = document.getElementById("uncompleted-counter");
+
 function updateCounters() {
   const completedTasks = document.querySelectorAll(".completed").length;
   const uncompletedTasks =
@@ -51,3 +49,27 @@ function updateCounters() {
   completedCounter.textContent = completedTasks;
   uncompletedCounter.textContent = uncompletedTasks;
 }
+
+checkbox.addEventListener("click", function () {
+  li.classList.toggle("completed", checkbox.checked);
+  //add the function below
+  updateCounters();
+});
+
+editBtn.addEventListener("click", function () {
+  const update = prompt("Edit task:", taskSpan.textContent);
+  if (update !== null) {
+    taskSpan.textContent = update;
+    li.classList.remove("completed");
+    //add the code below
+    checkbox.checked = false;
+    updateCounters();
+  }
+});
+
+deleteBtn.addEventListener("click", function () {
+  if (confirm("Are you sure you want to delete this task?")) {
+    li.remove();
+    updateCounters();
+  }
+});
