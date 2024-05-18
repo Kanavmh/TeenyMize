a7e8cbbbaeeb9f11d905b789e8f93fa6e853f784
Kanav Mahajan <kanavmahj@gmail.com>
2024-05-18 20:39:02 +0530

    Fixed bug
    -Task would not show as added, works now

--- Web/Tod/script.js
+++ Web/Tod/script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const li = document.createElement("li");
 
 function addTask() {
   const task = inputBox.value.trim();
